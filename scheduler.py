577d0600a93fe66bc793864c6d451ff987d94716
Pragnika Mancholaa <pragnikamancholaa@gmail.com>
2026-04-13 00:15:11 +0530

    Wrote jobs for both generate_data.py and etl_pipeline.py

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ import os
 
 target_days = 30
 
-def current_day():
+def get_current_day():
     db_path = "database/sales.db"
 
     if not os.path.exists(db_path):
@@ -16,6 +16,12 @@ def current_day():
     cursor = conn.cursor()
 
     try:
+
+        cursor.execute("""
+                       SELECT name FROM sqlite_master
+                       WHERE type='table' AND name='sales_clean'
+                       """)
+        
         if not cursor.fetchone():
             conn.close()
             return 0
@@ -30,4 +36,31 @@ def current_day():
         conn.close()
         return 0
     
+def generate_job():
+    current_day = get_current_day()
+
+    if current_day >= target_days:
+        print(f"Target of {target_days} days reached.")
+        return
+    
+    print(f"\n[SCHEDULER] Running generate_data.py - building Day {current_day + 1}....")
+    subprocess.run(["python", "data/generate_data.py"])
+
+def etl_job():
+    if not os.path.exists("data/today.csv"):
+        print("[SCHEDULER] today.csv not found")
+        return
+    
+    current_day = get_current_day()
+
+    if current_day >= target_days:
+        print(f"Target of {target_days} days reached.")
+        return
+    
+    print(f"[SCHEDULER] Running etl_pipeline.py - cleaning and loading {current_day + 1}....")
+    subprocess.run(["python", "pipeline/etl_pipeline.py"])
+
+    
+
+      
 
